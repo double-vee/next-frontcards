2c3e2e8f9151c312a3b3ecbf7b3fb12fdd4da301
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-04 22:22:23 +0100

    Render category links, update styles

--- src/components/CategorySection/CategorySection.jsx
+++ src/components/CategorySection/CategorySection.jsx
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+import { CATEGORIES } from '@/constants';
+
 import style from './CategorySection.module.css';
 
 export default function CategorySection() {
-  return <section className={style.categorySection}></section>;
+  return (
+    <section>
+      <ul
+        className={style.categoryList}
+        role="list"
+      >
+        {CATEGORIES.map(({ label, slug, className }) => (
+          <li
+            key={slug}
+            className={`
+            ${style.category}
+            ${style[className]}
+            `}
+          >
+            <Link href={`/${slug}`}>{label}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
---
+++ src/constants.jsx
@@ -0,0 +1,6 @@
+export const CATEGORIES = [
+  { label: 'JavaScript', slug: 'javascript', className: 'javascript' },
+  { label: 'string methods', slug: 'string-methods', className: 'methods' },
+  { label: 'array methods', slug: 'array-methods', className: 'methods' },
+  { label: 'React', slug: 'react', className: 'react' },
+];
