26a3d084b5b1ec5049be11dd797b2d06d2a491bc
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-05 02:38:09 +0100

    Move <main> styles to globals

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,8 @@
 import CategorySection from '@/components/CategorySection/CategorySection';
 
-import style from './page.module.css';
-
 export default function Home() {
   return (
-    <main className={style.wrapper}>
+    <main>
       <CategorySection />
     </main>
   );
