96c4cc4f5a3156b727ba676d7854f18aafb56f76
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-05 02:33:20 +0100

    Wrap category list in a nav tag

--- src/components/CategorySection/CategorySection.jsx
+++ src/components/CategorySection/CategorySection.jsx
@@ -6,22 +6,24 @@ import style from './CategorySection.module.css';
 export default function CategorySection() {
   return (
     <section>
-      <ul
-        className={style.categoryList}
-        role="list"
-      >
-        {CATEGORIES.map(({ label, slug, className }) => (
-          <li
-            key={slug}
-            className={`
+      <nav>
+        <ul
+          className={style.categoryList}
+          role="list"
+        >
+          {CATEGORIES.map(({ label, slug, className }) => (
+            <li
+              key={slug}
+              className={`
             ${style.category}
             ${style[className]}
             `}
-          >
-            <Link href={`/${slug}`}>{label}</Link>
-          </li>
-        ))}
-      </ul>
+            >
+              <Link href={`/${slug}`}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </section>
   );
 }
