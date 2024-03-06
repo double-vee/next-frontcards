3bc444e9ed310c9c27c52405612f4012d5fc1d43
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-06 04:34:43 +0100

    Rotate card on click with useState and CSS class toggle

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,28 @@
+'use client';
+
+import { useState } from 'react';
 import { CATEGORIES } from '@/constants';
 
 import style from './Card.module.css';
 
 export default function Card({ category, front, back }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
   const className = CATEGORIES.find((item) => item.slug === category).className;
 
+  const cardClassName = `${style.card} ${style[className]} ${
+    isFlipped ? style.rotate : undefined
+  }`;
+
   return (
-    <article>
-      <button className={`${style.card} ${style.front} ${style[className]}`}>
+    <article
+      className={style.wrapper}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      <button className={`${cardClassName} ${style.front}`}>
         <h2 className={style.cardTitle}>{front}</h2>
       </button>
-      <button className={`${style.card} ${style.back} ${style[className]}`}>
+      <button className={`${cardClassName} ${style.back}`}>
         <h2 className={style.cardTitle}>{front}</h2>
         <ul
           className={style.cardNotes}
