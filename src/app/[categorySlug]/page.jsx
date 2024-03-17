b5f461386142b2f45a64e4b59e544e7f13533b9d
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-17 04:08:50 +0100

    Add generateStaticParams() to statically generate routes at build time

--- src/app/[categorySlug]/page.jsx
+++ src/app/[categorySlug]/page.jsx
@@ -1,4 +1,9 @@
+import { CARDS } from '@/data';
 import CardGrid from '@/components/CardGrid/CardGrid';
+
+export function generateStaticParams() {
+  return CARDS.map((card) => ({ categorySlug: card.category }));
+}
 
 export default function CategoryPage({ params: { categorySlug } }) {
   return (
