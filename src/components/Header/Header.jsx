962fef25cc19a2775e7de8f9b9b4fe19429422ef
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-18 02:20:46 +0100

    Add link to the home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
+import Link from 'next/link';
 import style from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <h1 className={style.header__title}>Frontcards*</h1>
+      <h1 className={style.header__title}>
+        <Link
+          href={'/'}
+          aria-label="home page"
+        >
+          Frontcards*
+        </Link>
+      </h1>
       <p className={style.header__intro}>
         *Notes and snippets for wannabe frontend developers.
       </p>
