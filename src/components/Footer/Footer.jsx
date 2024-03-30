e6d2df692b05d1ed4fb82a00dd8e349ff2a7d4c8
Weronika Kansy <weronika.kansy@gmail.com>
2024-03-30 02:29:19 +0100

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <p>&copy; 2024 Created by a human developer.</p>
+      <p>&copy; 2024 double-vee. Created by a human developer.</p>
     </footer>
   );
 }
