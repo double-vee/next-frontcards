b5944d677349ad34d7335e4c38e45eee318351f1
Weronika Kansy <weronika.kansy@gmail.com>
2024-09-14 19:24:02 +0200

    Updated React notes and a screenshot

--- src/data/react/data.jsx
+++ src/data/react/data.jsx
@@ -3,8 +3,8 @@ export const CARDS = [
     category: 'react',
     front: 'props',
     back: [
-      'short for properties',
-      'used to pass values from parent components down to children',
+      'properties passed into a component (similar to arguments passed to a function)',
+      'used to pass data from parent components down to children',
       'to pass a value up to a parent, you first pass a function prop down to the child, and then call the function inside the child component',
       'read-only',
     ],
@@ -13,8 +13,9 @@ export const CARDS = [
     category: 'react',
     front: 'state',
     back: [
+      'used to track dynamic data',
       'managed within a component, cannot be accessed outside the component unless passed with props',
-      'mutable',
+      'updated immutably',
     ],
   },
 ];
